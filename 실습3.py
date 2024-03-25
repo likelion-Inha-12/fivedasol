84e68973931f5d2047c9864dd66cba99421d5845
dasol <5dasoll37@gmail.com>
2024-03-25 20:34:46 +0900

    파이썬 실습

---
+++ 실습3.py
@@ -0,0 +1,4 @@
+dasol=["백엔드","dasol","esfp",23]
+
+print("안녕하세요 저는 멋쟁이 사자처럼 %s파트이고\n이름은 %s입니다\n제 mbti는 %s이고\n나이는 %d살 입니다"
+%(dasol[0],dasol[1],dasol[2],dasol[3]))
---
+++ 실습4.py
@@ -0,0 +1,5 @@
+likelion={"이찬형":["통계학과",24,"entp"], "김시원":["산업경영공학과",23,"isfp"], "염종섭":["정보통신학과",25,"estp"]}
+
+print(likelion["이찬형"])
+print(likelion["김시원"])
+print(likelion["염종섭"])
