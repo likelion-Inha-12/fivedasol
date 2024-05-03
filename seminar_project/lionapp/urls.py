125147193a8d97a09f38562c5443e011d82759d1
dasol <5dasoll37@gmail.com>
2024-05-04 01:26:36 +0900

    DRF 기초 과제

--- seminar_project/lionapp/urls.py
+++ seminar_project/lionapp/urls.py
@@ -7,10 +7,12 @@ urlpatterns = [ # seminar_project/urls.py 에 lion/ 으로 지정했기 때문�
     path('create/', views.create_post),
     path('<int:pk>/', views.get_post), # pk라는 변수이름으로 맞춰줘야함
     # views.py 파일에 get_post에서 pk라고 정했기 때문인듯..?
-    path('delete/<int:pk>rak', views.delete_post),
+    path('delete/<int:pk>', views.delete_post),
     path('comments/<int:post_id>', views.get_comment),
     path('member/', views.create_member),
     path('like/<int:user_id>/<int:post_id>', views.like),
     path('getlike/<int:post_id>', views.get_likes),
-    path('sort', views.sort_post)
+    path('sort', views.sort_post),
+    path('v2/post/<int:pk>', views.PostApiView.as_view()),
+    path('v2/post', views.create_post_v2)
 ]
